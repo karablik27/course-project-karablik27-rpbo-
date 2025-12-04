5dda856ebb9f816330ac95f7e7164d0dd561e663
karabelnikov.s <karabelnikov.s@rwb.ru>
2025-12-04 22:44:11 +0300

    фикс

--- app/dummy_secret_test.py
+++ app/dummy_secret_test.py
@@ -1,4 +1,6 @@
 # ТЕСТОВЫЙ ЛОЖНЫЙ "СЕКРЕТ"
 # Этот токен НЕ является реальным, создан специально для тренировки Gitleaks.
 
-FAKE_API_TOKEN = "ghp_FAKEFAKEFAKEFAKEFAKE12345"  # noqa: S105 - fake token for Gitleaks training
+FAKE_API_TOKEN = (
+    "ghp_aB3dEfGHiJKlmnOPQRstUvWxYz012345678"  # noqa: S105 - fake token for Gitleaks training
+)
